9c98121c9e638f60caddf381942cf3a2ceb10b78
Jovan Trajkovic <jovan.trajkovic.003@gmail.com>
2025-11-02 19:39:45 +0100

    Improve GameContainer and make preview

--- composeApp/src/commonMain/kotlin/com/group83/a83/controller/GameContainerController.kt
+++ composeApp/src/commonMain/kotlin/com/group83/a83/controller/GameContainerController.kt
@@ -1,12 +1,18 @@
 package com.group83.a83.controller
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.group83.a83.model.GameQuestion
 
 class GameContainerController {
-    private val questions = listOf<GameQuestion>()
-    private var index = 0
+    private val questions = mutableListOf<GameQuestion>()
+    var index by mutableStateOf(0)
+        private set
 
-    fun getCurrentQuestion() = questions[index]
+    fun getListLength() = questions.lastIndex
+
+    fun getCurrentQuestion() = questions.getOrNull(index)
 
     fun next() {
         if (index < questions.lastIndex) index++
@@ -14,5 +20,11 @@ class GameContainerController {
 
     fun previous() {
         if (index > 0) index--
+    }
+
+    fun loadQuestions(newQuestions: List<GameQuestion>) {
+        questions.clear()
+        questions.addAll(newQuestions)
+        index = 0
     }
 }
--- composeApp/src/commonMain/kotlin/com/group83/a83/view/screens/GameContainerView.kt
+++ composeApp/src/commonMain/kotlin/com/group83/a83/view/screens/GameContainerView.kt
@@ -1,6 +1,6 @@
 package com.group83.a83.view.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import com.group83.a83.controller.GameContainerController
 import androidx.compose.runtime.Composable
 import com.group83.a83.controller.ABCDController
@@ -18,23 +18,70 @@ import com.group83.a83.view.component.FlashcardView
 import com.group83.a83.view.component.ImageABCDView
 import com.group83.a83.view.component.ImageInputView
 import com.group83.a83.view.component.InputView
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun GameContainerView(controller: GameContainerController) {
     val question = controller.getCurrentQuestion()
-
-    Column {
-        when (question) {
-            is FlashcardModel -> FlashcardView(FlashcardController(question))
-            is ABCDModel -> ABCDView(ABCDController(question))
-            is ImageABCDModel -> ImageABCDView(ImageABCDController(question))
-            is ImageInputModel -> ImageInputView(ImageInputController(question))
-            is InputModel -> InputView(InputController(question))
-        }
-
-//        NavigationControls(
-//            onPrev = { controller.previous() },
-//            onNext = { controller.next() }
-//        )
+    if (question == null) {
+        Text("No questions available.")
+        return
     }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(onClick = { controller.previous() }, enabled = controller.index > 0) {
+                Text("\u25C0", fontSize = 24.sp)
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                when (question) {
+                    is FlashcardModel -> FlashcardView(FlashcardController(question))
+                    is ABCDModel -> ABCDView(ABCDController(question))
+                    is ImageABCDModel -> ImageABCDView(ImageABCDController(question))
+                    is ImageInputModel -> ImageInputView(ImageInputController(question))
+                    is InputModel -> InputView(InputController(question))
+                }
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            IconButton(onClick = { controller.next() }, enabled = controller.index < controller.getListLength()) {
+                Text("\u25B6", fontSize = 24.sp)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GameContainerPreview() {
+    val questions = listOf(
+        FlashcardModel(
+            id = 1,
+            front = "What is the capital of France?",
+            back = "Paris"
+        ),
+        FlashcardModel(
+            id = 2,
+            front = "What is 2 + 2?",
+            back = "4"
+        )
+    )
+
+    val controller = GameContainerController()
+    controller.loadQuestions(questions)
+    GameContainerView(controller)
 }
