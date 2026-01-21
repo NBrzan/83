ed0b597da5427bf892b9bee2121d76a70ce8445a
NikBrzan <nik.brzan@student.um.si>
2026-01-21 13:32:13 +0100

    Centered game select screen elements

--- composeApp/src/commonMain/kotlin/com/group83/a83/view/screens/GameSelectView.kt
+++ composeApp/src/commonMain/kotlin/com/group83/a83/view/screens/GameSelectView.kt
@@ -64,16 +64,22 @@ fun GameSelectView(
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
             ) {
-                Column(modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.abcd) }) {
+                Column(
+                    modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.abcd) },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     SimpleGameDisplay(
                         "Več možnosti",
                         "https://cdn4.iconfinder.com/data/icons/top-search-7/128/_list_bullets_points_checklist_choose--512.png",
                         modifier = Modifier
                     )
                 }
-                Column(modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.input) }) {
+                Column(
+                    modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.input) },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     SimpleGameDisplay(
                         "Vnesite odgovor",
                         "https://cdn1.iconfinder.com/data/icons/radix/15/input-512.png",
@@ -86,15 +92,21 @@ fun GameSelectView(
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
             ) {
-                Column(modifier = Modifier.clickable { onGameSelected(GameType.imageABCD) }) {
+                Column(
+                    modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.imageABCD) },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     SimpleGameDisplay(
                         "Več možnosti za sliko",
                         "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-image-512.png"
                     )
                 }
-                Column(modifier = Modifier.clickable { onGameSelected(GameType.imageInput) }) {
+                Column(
+                    modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.imageInput) },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     SimpleGameDisplay(
                         "Odgovor na vnos slike",
                         "https://cdn4.iconfinder.com/data/icons/remixicon-media/24/image-edit-line-512.png"
@@ -107,12 +119,18 @@ fun GameSelectView(
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
             ) {
-                Column(modifier = Modifier.clickable { onGameSelected(GameType.flashcards) }) {
+                Column(
+                    modifier = Modifier.weight(1f).clickable { onGameSelected(GameType.flashcards) },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     SimpleGameDisplay("Flashcard Q/A", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F008%2F854%2F548%2Foriginal%2Fpoker-card-casino-3d-design-elements-free-png.png&f=1&nofb=1&ipt=dc7153b83d6a29256e18f2371cb24da4ca04dff1f772dfeb2925197d717819d4")
                 }
-                Column(modifier = Modifier.clickable { onWikipediaSelected() }) {
+                Column(
+                    modifier = Modifier.weight(1f).clickable { onWikipediaSelected() },
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     SimpleGameDisplay(
                         "Igra",
                         "https://cdn-icons-png.flaticon.com/512/10736/10736914.png"
